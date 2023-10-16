280cbfcdf07c1a401a38afdc7f3f05c286c68c10
Yotic <yoticcitoy@gmail.com>
2023-10-17 00:35:10 +0300

    Add project files.

---
+++ Cetours/ExternalFunctions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+public static unsafe class ExternalFunctions
+{
+    public static int strcmp(string st1, string st2)
+    {
+        int iST1 = 0, iST2 = 0;
+        for (int i = 0; i < (st1.Length > st2.Length ? st1.Length : st2.Length); i++)
+        {
+            iST1 += (i >= st1.Length ? 0 : st1[i]) - (i >= st2.Length ? 0 : st2[i]);
+            if (iST2 < 0)
+            {
+                if (iST1 < 0)
+                    iST2 += iST1;
+                if (iST1 > 0)
+                    iST2 += -iST1;
+            }
+            else
+            {
+                iST2 += iST1;
+            }
+        }
+        return iST2;
+    }
+
+    public static bool fullstrcmp(byte* p1, byte* p2, int length)
+    {
+        for (int i = 0; i < length; i++)
+            if (p1[i] != p2[i])
+                return false;
+        return true;
+    }
+}
---
+++ Cetours/Interop.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+public static unsafe class Interop
+{
+    const string user = "user32";
+    const string kernel = "kernel32";
+
+    [DllImport(kernel, CharSet = CharSet.Unicode)] public static extern
+        nint GetModuleHandle([MarshalAs(UnmanagedType.LPWStr)] string moduleName);
+
+    [DllImport(kernel)] public unsafe static extern 
+        nint VirtualQuery(byte* lpAddress, MEMORY_BASIC_INFORMATION* lpBuffer, int dwLength);
+
+    [DllImport(kernel)] public static extern 
+        bool VirtualProtectEx(nint hProcess, void* dwAddress, long nSize, int flNewProtect, int* lpflOldProtect);
+
+    [DllImport(kernel)] public static extern
+        nint GetCurrentProcess();
+
+    [DllImport(kernel)] public static extern 
+        int VirtualQueryEx(nint hProcess, void* lpAddress, MEMORY_BASIC_INFORMATION* lpBuffer, int dwLength);
+
+    [DllImport(kernel)] public static extern
+        bool VirtualFree(nint lpAddress, long dwSize, int dwFreeType);
+
+    [DllImport(kernel, ExactSpelling = true, EntryPoint = "RtlMoveMemory")] public static extern 
+        void CopyMemory(void* pdst, void* psrc, int cb);
+
+    [DllImport(kernel)] public static extern
+        bool VirtualProtect(void* lpAddress, long dwSize, int flNewProtect, int* lpflOldProtect);
+}
